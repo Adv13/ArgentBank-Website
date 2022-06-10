fd99c679c34f620504bec7a1698d84cee34fd2f5
Tanarys <tiffanydpro923@gmail.com>
2022-06-10 17:25:57 +0200

    maj presentation component

--- src/components/presentation/presentation.jsx
+++ src/components/presentation/presentation.jsx
@@ -4,7 +4,7 @@ import banner from "../../assets/bank-tree.jpeg"
 function Presentation() {
     return(
         <div className="presentation">
-            <img className="banner" src={banner} alt="Bannière photo"></img>
+            <img className="banner" src={banner} alt="Bannière"></img>
             <div className="textBanner">
                 <h1 className="title">No fees.<br></br>No minimum deposit.<br></br>High interest rates.</h1>
                 <h2 className="subtitle">Open a savings account with<br></br>Argent Bank today!</h2>
