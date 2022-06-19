4ff844e7b065e76ab53702d5e265ba12d2f60547
Adv <tiffanydpro923@gmail.com>
2022-06-19 16:30:22 +0200

    version configureStore added instead of createStore

--- src/store.js
+++ src/store.js
@@ -1,2 +1,10 @@
-import { createStore } from "redux";
-export const store = createStore();
+import { configureStore } from '@reduxjs/toolkit'
+import todosReducer from '../features/todos/todosSlice'
+import filtersReducer from '../features/filters/filtersSlice'
+
+export const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filtersReducer
+  }
+})
