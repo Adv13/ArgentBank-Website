dcc13c17cfa9deaa825ee2a5845f123750ed7a27
Adv <tiffanydpro923@gmail.com>
2022-06-20 17:36:48 +0200

    css problem fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,8 @@ import securityIcon from "../../assets/icon-security.png";
 function Home() {
   return (
     <main>
-      <div className="presentation">
-        <section className="presentation-content">
+      <div className="hero">
+        <section className="hero-content">
           <h2 className="sr-only">Promoted Content</h2>
           <p className="subtitle">No fees.</p>
           <p className="subtitle">No minimum deposit.</p>
