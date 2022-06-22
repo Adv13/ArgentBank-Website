61c8ee99d8bd1b96d61d8bb11f9265418d4cda9a
Adv <tiffanydpro923@gmail.com>
2022-06-22 15:38:56 +0200

    update profile feature done

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,20 +1,69 @@
 import { Link, useParams } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { saveProfile } from "../../store";
+import { updateProfile } from "../../API/api";
 /**
 * This function create the profile page based on the user's info recovered from the database.
 */
 function User() {
-  const { userId } = useParams();
+
+  //const { userId } = useParams();
+  const userDatas = useSelector((state) => state.profile);
+  const [edit, setEdit] = useState(false);
+  const dispatch = useDispatch();
+
+  async function save() {
+    try{
+      const firstName = document.querySelector(".firstNameInput");
+      const lastName = document.querySelector(".lastNameInput");
+      if (firstName.value === "" && lastName.value !== ""){
+        alert("Veuillez renseigner votre prénom.");
+      }else if (firstName.value !== "" && lastName.value === ""){
+        alert("Veuillez renseigner votre nom.");
+      }else if(firstName.value === "" && lastName.value === ""){
+        alert("Veuillez renseigner votre prénom et votre nom.");
+      }else{
+        const response = await updateProfile(firstName.value, lastName.value);
+        dispatch(saveProfile(response.data.body));
+        setEdit(false);
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <main className="main bg-dark">
-      <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          Tony Jarvis!
-        </h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
+      {edit ? (
+        <div className="header">
+          <h1>Welcome back</h1>
+          <div className="editor">
+            <input
+            type="text"
+            placeholder="{userDatas.firstName}"
+            className="firstNameInput"
+            />
+            <input
+            type="text"
+            placeholder="{userDatas.lastName}"
+            className="lastNameInput"
+            />
+          </div>
+          <div className="editor">
+            <button className="editor-button" onClick={save}>Save</button>
+            <button className="editor-button" onClick={()=>setEdit(false)}>Cancel</button>
+          </div>
+        </div>
+      ):(
+        <div className="header">
+          <h1>Welcome back
+          <br/>
+          {userDatas.firstName + " " + userDatas.lastName} !
+          </h1>
+          <button className="edit-button" onClick={()=>setEdit(true)}>Edit Name</button>
+        </div>
+      )}
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
         <div className="account-content-wrapper">
